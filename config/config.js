948dedd17c342d223e0ac5399f8f12344e9ff6bd
Your Name <you@example.com>
2022-08-23 21:21:57 +0800

    第二次合并，添加了微信登录

--- config/config.js
+++ config/config.js
@@ -2,9 +2,9 @@ const config = {
     // token秘钥
     TOKEN_SECRETKEY: "51aea2f7-9ff5-4acc-b633-40749d920de7",
     // 微信小程序APPID
-    WX_APPID: "wxd20152f73e4537d5",
+    WX_APPID: "wx7b85ab002ba2de66",
     // 微信小程序秘钥
-    WX_APPSECRET: "b370d2ae21eda0c4af5ba0381dd8acee",
+    WX_APPSECRET: "146d5f2acdd49fb9728717f502ec4053",
   };
   
   module.exports = config;
--- routers/wxapp/wecharlogin/wxlogin.js
+++ routers/wxapp/wecharlogin/wxlogin.js
@@ -3,17 +3,17 @@ const router = express.Router();
 // 使用https模块向微信服务器发送网络请求
 const https = require("https");
 
-const {WX_APPID,WX_APPSECRET}  = require('../../../config/config')
+const { WX_APPID, WX_APPSECRET } = require('../../../config/config')
 
 // 处理微信登录
-router.get("/wxlogin", (req, res) => {
+router.get("/wxlogin", (request, result) => {
   // 获取前端传过来的code值
-  const { code } = req.query;
+  const { code } = request.query;
+  console.log(code);
   // 微信小程序APPID
   const appid = WX_APPID;
   // 微信小程序秘钥
   const secret = WX_APPSECRET;
-
   // 使用https模块发送get网络请求
   https
     .get(
@@ -21,17 +21,27 @@ router.get("/wxlogin", (req, res) => {
       (res) => {
         let data = "";
         res.on("data", (chunk) => {
-          console.log("请求回来的数据：", chunk);
           data += chunk;
         });
         // 接收数据完毕
         res.on("end", () => {
-          console.log(JSON.parse(data));
+          // console.log("@@@：", JSON.parse(data));
           /* 
             openid：微信用户的唯一标识
             session_key：
           */
           const { openid, session_key } = JSON.parse(data);
+          console.log(openid, session_key);
+          let Data = {};
+          Data.openid = openid;
+          Data.session_key = session_key
+          // 生成token，返回给前端
+          result.send({
+            code: 0,
+            msg: "wx登录成功",
+            data: Data,
+          });
+
         });
       }
     )
